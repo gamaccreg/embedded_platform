7542271507ce4deecbf15b1667643a48d1054118
vane <vasi.nemes@gmail.com>
2019-03-16 13:18:39 +0200

    Added waf

---
+++ waf.cmd
@@ -0,0 +1,2 @@
+@set WAFDIR=%~dp0\__tools__\waf
+@%~dp0\__tools__\python27\python.exe %~dp0\__tools__\waf\waf-light %* & exit /b
---
+++ wscript
@@ -0,0 +1,3 @@
+import sys
+sys.path.append(r'__tools__\wscript')
+from wscript import *
